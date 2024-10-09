d97b82ba802f4d1985cb82c9cfca213864e1f903
Stefan <stefancool00@gmail.com>
2024-10-08 19:44:06 -0700

    Java project started

---
+++ unitcon/src/main/java/com/example/Main.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Nope!");
+
+        int x = 1 + 5;
+        System.out.println(x);
+    }
+}
